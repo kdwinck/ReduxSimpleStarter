edc32044267b0a003283e15a2b564843711e7105
Kyle Winckler <kyle.winckler@gmail.com>
2017-04-26 22:24:22 -0700

    create state on search bar class

--- src/components/search-bar.js
+++ src/components/search-bar.js
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { Component } from 'react' //same as const Component = React.Component
 
-const SearchBar = () => {
-  return <input />
+// CLASS COMPONTENT
+class SearchBar extends Component {
+
+  // this is used to initalize state in a class component | called whenever a new instance of a class is created
+  constructor(props) {
+    super(props)
+
+    this.state = { term: '' }
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={event => this.setState({ term: event.target.value })} />
+      </div>
+    )
+  }
+
 }
 
 export default SearchBar
+
+
+
+// FUNCTIONAL COMPONENT
+// const SearchBar = () => {
+//   return <input />
+// }
+
+// event listener
+// onInputChange(event) {
+//   console.log(event.target.value);
+// }
