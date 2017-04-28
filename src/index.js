b4ecebf6eb092fbe32fa0eee0d76ba2462918cf0
Kyle Winckler <kyle.winckler@gmail.com>
2017-04-27 19:46:48 -0700

    test api | convert app component to a class

--- src/index.js
+++ src/index.js
@@ -1,17 +1,34 @@
-import React from 'react' // const React = require('react') ??
+import React, { Component } from 'react' // const React = require('react') ??
 import ReactDOM from 'react-dom'
+// import dotenv from 'dotenv'
+import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search-bar'
 
-const API_KEY = process.env.API_KEY
+const API_KEY = 'AIzaSyDl0k7vDZbeVf9ZKSPFaa9EB5RQWOuyIHY'
+
+// dotenv.load()
+
+// const API_KEY = process.env.API_KEY
 
 // Create new component. It should produce HTML
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  )
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = { videos: [] }
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({ videos }) //es6 key and property have same variable name
+    })
+  }
+  render() {
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    )
+  }
 }
 
 // Take components generated HTML and put it in the DOM
