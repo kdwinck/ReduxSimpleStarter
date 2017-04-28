9207223d6cf90564d48397d095d433d19227894b
Kyle Winckler <kyle.winckler@gmail.com>
2017-04-27 20:54:13 -0700

    add video detail component

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import YTSearch from 'youtube-api-search'
 
 import SearchBar from './components/search-bar'
 import VideoList from './components/video-list'
+import VideoDetail from './components/video-detail'
 
 const API_KEY = 'AIzaSyDl0k7vDZbeVf9ZKSPFaa9EB5RQWOuyIHY'
 
@@ -12,7 +13,6 @@ const API_KEY = 'AIzaSyDl0k7vDZbeVf9ZKSPFaa9EB5RQWOuyIHY'
 
 // const API_KEY = process.env.API_KEY
 
-// Create new component. It should produce HTML
 class App extends Component {
   constructor(props) {
     super(props)
@@ -23,10 +23,12 @@ class App extends Component {
       this.setState({ videos }) //es6 key and property have same variable name
     })
   }
+
   render() {
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.videos[0]}/>
         <VideoList videos={this.state.videos}/>
       </div>
     )
